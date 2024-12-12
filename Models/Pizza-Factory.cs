1031f530fe0ee3cad7c9db8851dcddc5a633c1c2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-12-12 10:29:19 +0100

    Add files via upload

---
+++ Models/Pizza-Factory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PizzaFactory.Models
+{
+    public class Pizza
+    {
+        [JsonPropertyName("pizzaId")]
+        public int PizzaId { get; set; } = 0;
+
+        [JsonPropertyName("pizzaName")]
+        public string PizzaName { get; set; } = string.Empty;
+
+        [JsonPropertyName("size")]
+        public string Size { get; set; } = string.Empty;
+
+        [JsonPropertyName("ingredients")]
+        public List<string> Ingredients { get; set; } = new List<string>();
+
+        [JsonPropertyName("cookingMethod")]
+        public string CookingMethod { get; set; } = string.Empty;
+    }
+
+    public class PizzaCollection
+    {
+        [JsonPropertyName("pizzas")]
+        public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+    }
+}
